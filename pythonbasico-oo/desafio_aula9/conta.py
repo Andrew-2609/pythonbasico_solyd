49d1453a76202ee427e97b57c335434d81ba3abf
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-09-20 23:31:07 -0300

    small changes in conta.py methods

--- pythonbasico-oo/desafio_aula9/conta.py
+++ pythonbasico-oo/desafio_aula9/conta.py
@@ -5,7 +5,7 @@ class Conta:
         self.limite = limite
 
     def sacar(self, quantia):
-        if (self.saldo - quantia) > 0:
+        if (self.saldo - quantia) >= 0:
             print(f"Retirando R${quantia} do saldo. Saldo antes do saque: R${self.saldo}.")
             self.saldo -= quantia
             print(f"Saldo após o saque: R${self.saldo}")
@@ -13,7 +13,7 @@ class Conta:
             print("Operação negada! Seu saldo ficaria menor que R$0!")
 
     def depositar(self, quantia):
-        if (self.saldo + quantia) < self.limite:
+        if (self.saldo + quantia) <= self.limite:
             print(f"Adicionando R${quantia} ao seu saldo. Saldo antes do depósito: R${self.saldo}")
             self.saldo += quantia
             print(f"Saldo após o depósito: R${self.saldo}")
@@ -21,4 +21,4 @@ class Conta:
             print(f"Operação negada! Seu saldo ficaria maior que seu limite de R${self.limite}!")
 
     def consultar_saldo(self):
-        print(f"O saldo do cliente {self.cliente} é de R${self.saldo}")
+        return f"O saldo do cliente {self.cliente.nome} é de R${self.saldo}"
