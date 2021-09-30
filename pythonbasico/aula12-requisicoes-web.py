e1a54b108498dc044627406b6477d2e7627f6f33
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-09-29 21:36:58 -0300

    correction of foreignisms in aula12-requisicoes-web.py

--- pythonbasico/aula12-requisicoes-web.py
+++ pythonbasico/aula12-requisicoes-web.py
@@ -1,15 +1,15 @@
 import requests
 
 
-def clone_index_page():
+def clonar_pagina_principal():
     endereco = input("De qual site você deseja obter o clone da página inicial?\n")
 
     try:
         requisicao = requests.get(endereco)
 
-        file = open("../index.html", 'w')
-        file.write(requisicao.text)
-        file.close()
+        arquivo = open("../index.html", 'w')
+        arquivo.write(requisicao.text)
+        arquivo.close()
 
         print("O arquivo index.html pode ser encontrado na raiz desse projeto.")
     except Exception as err:
