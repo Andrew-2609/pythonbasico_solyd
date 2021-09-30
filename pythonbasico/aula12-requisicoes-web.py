bed85059dc9e13316a53fdd2f6e43db16dcc3e32
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-09-29 23:07:37 -0300

    simple GET requests implemented in aula12-requisicoes-web.py with headers and cookies

--- pythonbasico/aula12-requisicoes-web.py
+++ pythonbasico/aula12-requisicoes-web.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 
 
@@ -14,3 +16,25 @@ def clonar_pagina_principal():
         print("O arquivo index.html pode ser encontrado na raiz desse projeto.")
     except Exception as err:
         print(f"Não foi possível fazer requisição no endereço {endereco}. Erro: {err}")
+
+
+def requisicao_get(endereco, cabecalhos, cookies):
+    requisicao = None
+
+    try:
+        requisicao = requests.get(endereco, headers=cabecalhos, cookies=cookies)
+        return requisicao.text
+    except Exception as err:
+        print(f"Não foi possível realizar a requisição para {requisicao}. Erro: {err}")
+
+
+if __name__ == '__main__':
+    meus_cabecalhos = {"user-agent": "Windows 15"}
+    meus_cookies = {"ultima-visita": str(datetime.datetime.now())}
+    endereco_requisicao = input("Digite um endereço para fazer a requisição GET: ")
+
+    resposta = requisicao_get(endereco_requisicao, meus_cabecalhos, meus_cookies)
+
+    print(resposta)
+
+    print("\n\n", "#" * 7, "Fim da execução", "#" * 7)
