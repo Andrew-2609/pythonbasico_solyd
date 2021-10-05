2626d1dec989d49b6782674a56b6a1ab36e98050
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-05 08:55:10 -0300

    Add the logic for searching and printing movie details in a loop

--- pythonbasico/aula13-api-filmes.py
+++ pythonbasico/aula13-api-filmes.py
@@ -13,15 +13,32 @@ def carregar_api_key():
 
 def retornar_dados_do_filme(titulo):
     try:
-        req = requests.get(f"https://www.omdbapi.com/?t={titulo}&apikey={carregar_api_key()}")
-        return req
+        req = requests.get(f"https://www.omdbapi.com/?t={titulo}&apikey={carregar_api_key()}&type=movie")
+        dict_filme = json.loads(req.text)
+        return dict_filme
     except Exception as err:
         print("Connection error:", err)
         exit()
 
 
-filme = input("De qual filme você deseja saber os dados? Informe: ")
+def printar_resumo_do_filme(filme):
+    print("\n" + ("#" * 4))
+    print("Título:", filme['Title'])
+    print("Ano:", filme['Year'])
+    print("Gênero:", filme['Genre'])
+    print("Diretor:", filme['Director'])
+    print("Atores:", filme['Actors'])
+    print("Sinopse:", filme['Plot'])
+    print(("#" * 4) + "\n")
 
-dicionario = json.loads(retornar_dados_do_filme(filme).text)
 
-print(dicionario)
+sair = False
+while not sair:
+    opcao = input("Escreva o nome de um filme para retornar seus dados, ou SAIR para sair do programa: ")
+
+    if opcao == "SAIR":
+        print("Saindo...")
+        sair = True
+    else:
+        filme_selecionado = retornar_dados_do_filme(opcao)
+        printar_resumo_do_filme(filme_selecionado)
