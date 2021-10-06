d572bc82d11d7652bf381d37a1fe4cc8a23a129f
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-05 23:05:06 -0300

    Implement search for emails on a website page with regex

--- pythonbasico/aula14-expressoes-regulares.py
+++ pythonbasico/aula14-expressoes-regulares.py
@@ -1,12 +1,25 @@
 import re
+import requests
 
-meu_regex = r"[\w.-]+@[\w-]+\.[\w.-]+"
 
-lista_de_teste = ["ndrewcoding@ndog.com", "brazil.com", "jonathan.harker@gmail.com"]
+def retorna_registros_por_padrao(padrao_regex, string_de_busca):
+    print("# Procurando por padrão...")
+    padroes = re.findall(padrao_regex, string_de_busca)
+    if padroes:
+        print("## Sucesso!\nRegistros encontrados:")
+        return set(padroes)
+    else:
+        return "Esse padrão não foi encontrado dentro da String passada =(."
 
-padroes = re.findall(meu_regex, str(lista_de_teste))
 
-if padroes:
-    print(padroes)
-else:
-    print("Padrão não encontrado...")
+if __name__ == '__main__':
+    meu_regex = r"[\w.-]+@[\w-]+\.[\w.-]+"
+    requisicao_web = None
+    try:
+        requisicao_web = requests.get("https://www.loucosporcoxinha.com.br/contato/")
+    except Exception as err:
+        print(f"A requisição falhou. Erro: {err}")
+        exit()
+
+    registros = retorna_registros_por_padrao(meu_regex, requisicao_web.text)
+    print(registros)
