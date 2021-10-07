a64baaa0b758c7ced0323fc3ada1bd34eb420134
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-07 10:08:15 -0300

    Feat: implement the challenge of getting weather data from a city via API

---
+++ pythonbasico/aula15-desafio-api-clima.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import datetime as dt
+
+apiKey = json.load(open("apiKey.json"))["api-clima"]
+
+
+def buscar_informacoes_climaticas(cidade):
+    try:
+        req = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={apiKey}&units=metric&lang=pt_br")
+        return json.loads(req.text)
+    except Exception as err:
+        print(f"Falha na requisição. Erro: {err}")
+        exit()
+
+
+def organizar_e_mostrar_resposta(res):
+    print("\n###")
+    if not res['message']:
+        horario_nascer_sol = dt.datetime.utcfromtimestamp(int(res['sys']['sunrise'])).strftime('%H:%M:%S')
+        horario_por_sol = dt.datetime.utcfromtimestamp(int(res['sys']['sunset'])).strftime('%H:%M:%S')
+        print("Nome oficial da cidade:", res['name'])
+        print("País:", res['sys']['country'])
+        print("Clima:", str.capitalize(res['weather'][0]['description']))
+        print("Temperatura estimada:", str(res['main']['temp']) + "°C")
+        print("Sensação térmica:", str(res['main']['feels_like']) + "°C")
+        print("Horário do nascer do Sol (UTC):", horario_nascer_sol)
+        print("Horário do pôr do Sol (UTC):", horario_por_sol)
+    else:
+        print(f"Erro {res['cod']}:", str.capitalize(res['message']))
+    print("###\n")
+
+
+if __name__ == '__main__':
+    opcao = input("De qual cidade você deseja saber informações climáticas? Digite: ")
+    resposta = buscar_informacoes_climaticas(opcao)
+    organizar_e_mostrar_resposta(resposta)
