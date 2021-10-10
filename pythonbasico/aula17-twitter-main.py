618f186fedceaf44a18a165bb902e9ec79a6d0a6
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-10 03:15:25 -0300

    Refactor: change the call for the api_key dict key in aula17-twitter-main.py

--- pythonbasico/aula17-twitter-main.py
+++ pythonbasico/aula17-twitter-main.py
@@ -3,10 +3,10 @@ from twitter import Twitter
 
 if __name__ == '__main__':
     api_key = ApiKey.return_api_key_file()
-    consumer_key = api_key['consumer_key']
-    consumer_secret = api_key['consumer_secret']
-    access_token = api_key['access_token']
-    access_token_secret = api_key['access_token_secret']
+    consumer_key = api_key.get("consumer_key")
+    consumer_secret = api_key.get("consumer_secret")
+    access_token = api_key.get("access_token")
+    access_token_secret = api_key.get("access_token_secret")
 
     twitter = Twitter(consumer_key, consumer_secret, access_token, access_token_secret)
 
