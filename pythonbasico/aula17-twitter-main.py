ada49c797b01867a72e2465c365c903ef817efac
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-10 02:55:07 -0300

    Chore: insert the main code inside a main function and print the search result

--- pythonbasico/aula17-twitter-main.py
+++ pythonbasico/aula17-twitter-main.py
@@ -1,18 +1,19 @@
 from twitter import Twitter
 import json
 
-api_key = None
-try:
-    api_key = json.load(open("apiKey.json"))
-except Exception as err:
-    print("Ocorreu um erro ao abrir o arquivo:", err)
-    exit()
+if __name__ == '__main__':
+    api_key = None
+    try:
+        api_key = json.load(open("apiKey.json"))
+    except Exception as err:
+        print("Ocorreu um erro ao abrir o arquivo:", err)
+        exit()
 
-consumer_key = api_key['consumer_key']
-consumer_secret = api_key['consumer_secret']
-access_token = api_key['access_token']
-access_token_secret = api_key['access_token_secret']
+    consumer_key = api_key['consumer_key']
+    consumer_secret = api_key['consumer_secret']
+    access_token = api_key['access_token']
+    access_token_secret = api_key['access_token_secret']
 
-twitter = Twitter(consumer_key, consumer_secret, access_token, access_token_secret)
+    twitter = Twitter(consumer_key, consumer_secret, access_token, access_token_secret)
 
-twitter.tweet("Tweet feito com classe Twitter usando API")
+    print(twitter.search_tweets("#nba", "de"))
