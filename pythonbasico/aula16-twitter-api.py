73d08f5fa3c980d4df12129ab921dc313e4fa070
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-09 23:02:55 -0300

    Refactor: fragment the code in separate defs

--- pythonbasico/aula16-twitter-api.py
+++ pythonbasico/aula16-twitter-api.py
@@ -1,24 +1,35 @@
 import oauth2
 import json
+import pprint
 
-try:
-    apiKey = json.load(open("apiKey.json"))
-except Exception as err:
-    print("Ocorreu um erro ao abrir o arquivo:", err)
-else:
+
+def realizar_autorizacao_retornar_client():
+    try:
+        api_key = json.load(open("apiKey.json"))
+    except Exception as err:
+        print("Ocorreu um erro ao abrir o arquivo:", err)
+    else:
+        consumer_key = api_key['consumer_key']
+        consumer_secret = api_key['consumer_secret']
+        access_token = api_key['access_token']
+        access_token_secret = api_key['access_token_secret']
+
+        consumer = oauth2.Consumer(consumer_key, consumer_secret)
+        token = oauth2.Token(access_token, access_token_secret)
+
+        client = oauth2.Client(consumer, token)
+        return client
+
+
+def retornar_corpo_de_tweets(palavra_chave):
+    res_client = realizar_autorizacao_retornar_client()
     base_url = "https://api.twitter.com/1.1/search/tweets.json"
-    consumer_key = apiKey['consumer_key']
-    consumer_secret = apiKey['consumer_secret']
-    access_token = apiKey['access_token']
-    access_token_secret = apiKey['access_token_secret']
 
-    consumer = oauth2.Consumer(consumer_key, consumer_secret)
-    token = oauth2.Token(access_token, access_token_secret)
+    corpo_resposta = res_client.request(f"{base_url}?q={palavra_chave}")[1]
+    corpo_decodificado = corpo_resposta.decode()
+    corpo_dict = json.loads(corpo_decodificado)
+    return corpo_dict
 
-    client = oauth2.Client(consumer, token)
 
-    req = client.request(f"{base_url}?q=brasil")[1]
-    req_decode = req.decode()
-    req_obj = json.loads(req_decode)
-
-    print(req_obj)
+if __name__ == '__main__':
+    pprint.pprint(retornar_corpo_de_tweets("brasil"))
